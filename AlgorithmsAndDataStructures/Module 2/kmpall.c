f377fa121b813f9a1263eb9fff5e4b3464c38f7f
Jidowski <tarasovalx99@gmail.com>
2019-02-16 23:03:58 +0300

    Create kmpall.c

---
+++ AlgorithmsAndDataStructures/Module 2/kmpall.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+int *Prefix(char *s){
+    int slen = strlen(s);
+    int *p = malloc(sizeof(int)* slen);
+    p[0] = 0;
+    int t = 0, i = 1;
+    for (i; i < slen; i++){
+        for (t; t > 0 && (s[t] != s[i]); t = p[t - 1]);
+        if (s[t] == s[i])
+            t++;
+        p[i] = t;
+    }
+    return p;
+}
+
+void KMP(char* S, char* T){
+    int slen = strlen(S);
+    int tlen = strlen(T);
+    int *prefs = Prefix(S);
+    int q = 0, k = 0;
+    for(;k < tlen; k++){
+        for(;q > 0 && S[q] != T[k]; q = prefs[q - 1]);
+        if (S[q] == T[k])
+            q++;
+        if (q == slen) {
+            k = k - slen + 1;
+            printf("%d ", k);
+            k += slen - 1;
+        }
+    }
+    free(prefs);
+}
+
+int main(int argc, char **argv) {
+    KMP(argv[1], argv[2]);
+}
